629fc01306f3422643a8d1667b11361f2cc503c1
Harald Blikø <haraldbliko@cmtr.io>
2023-01-01 22:22:43 +0100

    move graph test to generic artifaactTest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const date = require("./date");
 const graph = require("./graph");
 const math = require("./math");
-const io =. require("./io");
+const io = require("./io");
 
 module.exports = {
 	...date,
